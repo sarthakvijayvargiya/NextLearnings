81cceb88e99880ef5ce036510ac522fee59b1440
sarthakvijayvargiya <vsarthakvijay@gmail.com>
2024-07-14 22:39:09 +0530

    feat: updated

--- chaiaurcodetwofullstack/mystrymessage/src/app/layout.tsx
+++ chaiaurcodetwofullstack/mystrymessage/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,13 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body className={inter.className}>
-        <div className="relative flex items-center justify-center w-full">
-          <Navbar />
-        </div>
-        {children}
-      </body>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
